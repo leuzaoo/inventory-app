2c22e005d6123453815131885da55ab3c853dc1e
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-06 09:36:26 -0300

    Updated sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">LEOSTOCK</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* links */}
+      <div className="flex-grow mt-8">{/* links here */}</div>
+      {/* footer */}
+      <div>
+        <p className="text-center text-xs text-gray-500ray500">
+          &copy; 2024 LEOSTOCK
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
