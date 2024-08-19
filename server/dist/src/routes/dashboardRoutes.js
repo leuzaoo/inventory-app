06de647220187a02df593ae3be5348c0867855e1
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-19 09:17:28 -0300

    Stop tracking server/dist directory

--- server/dist/src/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dashboardController_1 = require("../controllers/dashboardController");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/", dashboardController_1.getDashboardMetrics);
-exports.default = router;
