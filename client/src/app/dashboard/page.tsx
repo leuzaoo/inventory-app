ff3378a92692c58023a0c992fcc9a23eb34c73c7
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-20 09:47:29 -0300

    Added the basic to start up the frontend

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -3,7 +3,13 @@
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
-      Dashboard
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
+      <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" />
+      <div className="row-span-3 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
     </div>
   );
 };
