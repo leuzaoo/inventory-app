12025d756fa9e1e00f4501378a1f19fbddc7772a
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-20 09:28:52 -0300

    Update to not tracking .next directory file

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      Dashboard
+    </div>
+  );
 };
 
 export default Dashboard;
