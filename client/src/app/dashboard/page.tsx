0046818c8e8478c3017b5bcc00542274c9c011dd
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-21 10:25:33 -0300

    Updated dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import CardPopularProducts from "./CardPopularProducts";
+import CardSalesSummary from "./CardSalesSummary";
 
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
+      <CardSalesSummary />
       <div className="row-span-3 xl:row-span-6 bg-gray-500" />
       <div className="row-span-3 xl:row-span-6 bg-gray-500" />
       <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500" />
