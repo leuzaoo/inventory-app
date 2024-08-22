f4db0ea31141d5d3c6542bc9515f58b7b2d78cf4
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-22 11:43:20 -0300

    Updated dashboard page

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
 import CardPurchaseSummary from "./CardPurchaseSummary";
 import CardSalesSummary from "./CardSalesSummary";
@@ -8,10 +9,8 @@ const Dashboard = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
       <CardSalesSummary />
-      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
-      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
       <CardPurchaseSummary />
-      <div className="row-span-3 bg-gray-500" />
+      <CardExpenseSummary />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
