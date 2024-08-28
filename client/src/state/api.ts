c87bbfbc7520916e400504a1fbaff564f95e826d
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-27 23:15:55 -0300

    Updated backend api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -50,10 +50,16 @@ export interface DashboardMetrics {
   expenseByCategorySummary: ExpenseByCategorySummary[];
 }
 
+export interface User {
+  userId: string;
+  name: string;
+  email: string;
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: ["DashboardMetrics", "Products"],
+  tagTypes: ["DashboardMetrics", "Products", "Users"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
@@ -73,6 +79,10 @@ export const api = createApi({
         body: newProduct,
       }),
       invalidatesTags: ["Products"],
+    }),
+    getUsers: build.query<User[], void>({
+      query: () => "/users",
+      providesTags: ["Users"],
     }),
   }),
 });
