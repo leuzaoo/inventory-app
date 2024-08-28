44c967214d59b3fe024ff878b1c3aaa9fa63c79b
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-28 09:58:55 -0300

    Updated port app

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,7 @@ app.use("/users", userRoutes);
 
 // SERVER
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
